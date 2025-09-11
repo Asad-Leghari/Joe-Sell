68fe45a53d92d4cf6643779cb71e96fc79ee014a
TFKOrig <bhatkhans7@gmail.com>
2025-09-12 00:59:57 +0500

    Add comment to clarify potential double routing issues in Navbar component

--- src/frontend/core/components/Navbar.tsx
+++ src/frontend/core/components/Navbar.tsx
@@ -82,7 +82,7 @@ const Navbar = () => {
           </div>
         )}
       </div>
-
+      {/* double routing causes issues so fallbacks to one time route  */}
       <SignInModal
         isOpen={isLoginOpen}
         onClose={() => setIsLoginOpen(false)}
